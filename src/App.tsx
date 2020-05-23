95cfd1a2dc67d547d511bb9132f8a0d8b53e7681
Kelly Bondanza <kelly.bondanza@gmail.com>
2020-05-23 13:01:41 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  color: blue;
+`;
+
+function App() {
+  return <Container>Todo</Container>;
+}
+
+export default App;
