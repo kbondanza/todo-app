f80ff82f0dddc39f5c9dd2900f61e86480120dd5
Kelly Bondanza <kelly.bondanza@gmail.com>
2020-05-23 22:34:48 -0400

    finished recoil todo app tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+// RecoilRoot provides the context in which atoms have values
+import { RecoilRoot } from "recoil";
 import styled from "styled-components";
+import TodoList from "./components/TodoList";
 
 const Container = styled.div`
   color: blue;
 `;
 
 function App() {
-  return <Container>Todo</Container>;
+  return (
+    <RecoilRoot>
+      <Container>Todo</Container>
+      <TodoList />
+    </RecoilRoot>
+  );
 }
 
 export default App;
